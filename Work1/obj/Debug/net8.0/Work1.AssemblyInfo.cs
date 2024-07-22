d5b5609b91d21587990d475108198c581f98bedd
APCEH-M <apceh_@mail.ru>
2024-07-22 10:05:36 +0500

    Обновление

--- Work1/obj/Debug/net8.0/Work1.AssemblyInfo.cs
+++ Work1/obj/Debug/net8.0/Work1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Work1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7cbea61ad6908331390ab9063dd66d178f393f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Work1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Work1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
